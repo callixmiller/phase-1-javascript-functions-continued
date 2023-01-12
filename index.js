967e392738c3332f4ab3217d7a7d8ca1353850ac
callixmiller <calmill413@gmail.com>
2023-01-12 14:55:38 -0500

    index.js functions all passing

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return "This Saturday, I want to " + activity +"!";
+}
+
+saturdayFun('bathe my dog');
+
+function mondayWork(work = 'go to the office') {
+    return "This Monday, I will " + work +".";
+}
+
+mondayWork("work from home");
+
+function wrapAdjective(special) {
+    return function(name) {
+        return `You are ${special}${name}${special}!`;
+    }
+}
